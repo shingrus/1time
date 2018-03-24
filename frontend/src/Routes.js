6acb00051522b54859b6090b9fa017a226140dd5
shingrus <shingrus@gmail.com>
2018-03-25 02:45:55 +0300

    index.html

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import NewMessage from "./containers/NewMessage";
 import ShowNewLink from "./containers/ShowNewLink";
 import ViewSecretMessage from "./containers/ViewSecretMessage"
@@ -9,9 +9,9 @@ import ViewError from './containers/ErrorComponent';
 export default () =>
     <Switch>
 
-        <Route path="/" exact component={NewMessage} />
-        <Route path="/index.html" exact component={NewMessage} />
-        <Route path="/new" ex    component={ShowNewLink} />
-        <Route path="/v/(.*)"  component={ViewSecretMessage} />
-        <Route component={ViewError} />
+        <Route path="/" exact component={NewMessage}/>
+        <Route path="/index.html" exact component={NewMessage}/>
+        <Route path="/new" component={ShowNewLink}/>
+        <Route path="/v/(.*)" component={ViewSecretMessage}/>
+        <Route component={ViewError}/>
     </Switch>;
